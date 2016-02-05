45e63073096c12b2942301939be7797eb41a36f4
Stefan Lau <github@stefanlau.com>
2016-02-05 19:28:13 +0100

    Upgrade to eslint >= 1.0.0

--- test/rules/filenames.js
+++ test/rules/filenames.js
@@ -1,11 +1,11 @@
-var linter = require("eslint").linter,
-    ESLintTester = require("eslint-tester");
+var filenamesRule = require('../../lib/rules/filenames'),
+    RuleTester = require("eslint").RuleTester;
 
-var eslintTester = new ESLintTester(linter),
-    testCode = "var foo = 'bar';",
-    testExportingCode = "module.exports = exported;";
+var testCode = "var foo = 'bar';",
+    testExportingCode = "module.exports = exported;",
+    ruleTester = new RuleTester();
 
-eslintTester.addRuleTest("lib/rules/filenames", {
+ruleTester.run("lib/rules/filenames", filenamesRule, {
 
     valid: [
         {
@@ -31,82 +31,82 @@ eslintTester.addRuleTest("lib/rules/filenames", {
         {
             code: testCode,
             filename: "foo_bar.js",
-            args: [ 1, "^[a-z_]+$" ]
+            options: [ "^[a-z_]+$" ]
         },
         {
             code: testCode,
             filename: "exported.js",
-            args: [ 1, null, "match-regex-and-exported" ]
+            options: [ null, "match-regex-and-exported" ]
         },
         {
             code: testCode,
             filename: "exported.js",
-            args: [ 1, null, "match-regex-or-exported" ]
+            options: [ null, "match-regex-or-exported" ]
         },
         {
             code: testCode,
             filename: "exported.js",
-            args: [ 1, null, "match-exported-or-regex" ]
+            options: [ null, "match-exported-or-regex" ]
         },
         {
             code: testExportingCode,
             filename: "exported.js",
-            args: [ 1, null, "match-regex-and-exported" ]
+            options: [ null, "match-regex-and-exported" ]
         },
         {
             code: testExportingCode,
             filename: "bar.js",
-            args: [ 1, null, "match-regex-or-exported" ]
+            options: [ null, "match-regex-or-exported" ]
         },
         {
             code: testExportingCode,
             filename: "exported.js",
-            args: [ 1, "^a$", "match-exported-or-regex" ]
+            options: [ "^a$", "match-exported-or-regex" ]
         },
 
         // Testing exported name extraction
         {
             code: "module.exports = foo;",
             filename: "foo.js",
-            args: [ 1, "^", "match-exported-and-regex" ],
+            options: [ "^", "match-exported-and-regex" ],
             ecmaFeatures: { modules: true }
         },
         {
             code: "module.exports = class Foo {};",
             filename: "Foo.js",
-            args: [ 1, "^", "match-exported-and-regex" ],
+            options: [ "^", "match-exported-and-regex" ],
             ecmaFeatures: { modules: true, classes: true }
         },
         {
             code: "module.exports = function foo() {}",
             filename: "foo.js",
-            args: [ 1, "^", "match-exported-and-regex" ],
+            options: [ "^", "match-exported-and-regex" ],
             ecmaFeatures: { modules: true }
         },
         {
             code: "export default foo;",
             filename: "foo.js",
-            args: [ 1, "^", "match-exported-and-regex" ],
+            options: [ "^", "match-exported-and-regex" ],
             ecmaFeatures: { modules: true }
         },
         {
             code: "export default class Foo {}",
             filename: "Foo.js",
-            args: [ 1, "^", "match-exported-and-regex" ],
+            options: [ "^", "match-exported-and-regex" ],
             ecmaFeatures: { modules: true, classes: true }
         },
         {
             code: "export default function foo() {}",
             filename: "foo.js",
-            args: [ 1, "^", "match-exported-and-regex" ],
+            options: [ "^", "match-exported-and-regex" ],
             ecmaFeatures: { modules: true }
         },
         {
             code: "export default function () {}", // No exported name
             filename: "foo.js",
-            args: [ 1, "^", "match-exported-and-regex" ],
+            options: [ "^", "match-exported-and-regex" ],
             ecmaFeatures: { modules: true }
-        },
+        }
     ],
 
     invalid: [
@@ -114,70 +114,70 @@ eslintTester.addRuleTest("lib/rules/filenames", {
             code: testCode,
             filename: "foo_bar.js",
             errors: [
-                { message: "Filename 'foo_bar.js' does not match the naming convention.", column: 0, line: 0 }
+                { message: "Filename 'foo_bar.js' does not match the naming convention.", column: 1, line: 1 }
             ]
         },
         {
             code: testCode,
             filename: "fooBAR.js",
             errors: [
-                { message: "Filename 'fooBAR.js' does not match the naming convention.", column: 0, line: 0 }
+                { message: "Filename 'fooBAR.js' does not match the naming convention.", column: 1, line: 1 }
             ]
         },
         {
             code: testCode,
             filename: "fooBar$.js",
             errors: [
-                { message: "Filename 'fooBar$.js' does not match the naming convention.", column: 0, line: 0 }
+                { message: "Filename 'fooBar$.js' does not match the naming convention.", column: 1, line: 1 }
             ]
         },
         {
             code: testCode,
             filename: "fooBar.js",
-            args: [ 1, "^[a-z_]$" ],
+            options: [ "^[a-z_]$" ],
             errors: [
-                { message: "Filename 'fooBar.js' does not match the naming convention.", column: 0, line: 0 }
+                { message: "Filename 'fooBar.js' does not match the naming convention.", column: 1, line: 1 }
             ]
         },
         {
             code: testExportingCode,
             filename: "fooBar.js",
-            args: [ 1, null, "match-regex-and-exported" ],
+            options: [ null, "match-regex-and-exported" ],
             errors: [
-                { message: "File 'fooBar.js' must be named 'exported.js', after its exported value.", column: 0, line: 0 }
+                { message: "File 'fooBar.js' must be named 'exported.js', after its exported value.", column: 1, line: 1 }
             ]
         },
         {
             code: testExportingCode,
             filename: "foo.js",
-            args: [ 1, "^[a-z_]$", "match-regex-and-exported" ],
+            options: [ "^[a-z_]$", "match-regex-and-exported" ],
             errors: [
-                { message: "Filename 'foo.js' does not match the naming convention.", column: 0, line: 0 },
-                { message: "File 'foo.js' must be named 'exported.js', after its exported value.", column: 0, line: 0 }
+                { message: "Filename 'foo.js' does not match the naming convention.", column: 1, line: 1 },
+                { message: "File 'foo.js' must be named 'exported.js', after its exported value.", column: 1, line: 1 }
             ]
         },
         {
             code: testExportingCode,
             filename: "foo.js",
-            args: [ 1, "^a$", "match-regex-or-exported" ],
+            options: [ "^a$", "match-regex-or-exported" ],
             errors: [
-                { message: "Filename 'foo.js' does not match the naming convention nor exported value 'exported'.", column: 0, line: 0 }
+                { message: "Filename 'foo.js' does not match the naming convention nor exported value 'exported'.", column: 1, line: 1 }
             ]
         },
         {
             code: testExportingCode,
             filename: "foo.js",
-            args: [ 1, null, "match-exported-or-regex" ],
+            options: [ null, "match-exported-or-regex" ],
             errors: [
-                { message: "File 'foo.js' must be named 'exported.js', after its exported value.", column: 0, line: 0 }
+                { message: "File 'foo.js' must be named 'exported.js', after its exported value.", column: 1, line: 1 }
             ]
         },
         {
             code: testCode,
             filename: "foo.js",
-            args: [ 1, "^[a-z_]$", "match-exported-or-regex" ],
+            options: [ "^[a-z_]$", "match-exported-or-regex" ],
             errors: [
-                { message: "Filename 'foo.js' does not match the naming convention.", column: 0, line: 0 }
+                { message: "Filename 'foo.js' does not match the naming convention.", column: 1, line: 1 }
             ]
         }
     ]
