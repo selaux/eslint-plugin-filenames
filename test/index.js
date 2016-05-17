5ab1f4477643ea8bbecd07b1f003993a975de80d
Stefan Lau <github@stefanlau.com>
2016-05-17 19:07:51 +0200

    Remove unneccessary mocha require.

--- test/index.js
+++ test/index.js
@@ -1,22 +1,21 @@
-var mocha = require("mocha"),
-    expect = require("chai").expect,
+var expect = require("chai").expect,
     index = require("../index.js"),
     matchRegex = require("../lib/rules/match-regex"),
     matchExported = require("../lib/rules/match-exported"),
     noIndex = require("../lib/rules/no-index");
 
-mocha.describe("index.js", function () {
+describe("index.js", function () {
     "use strict";
 
-    mocha.it("should export the match-regex rule", function () {
+    it("should export the match-regex rule", function () {
         expect(index.rules['match-regex']).to.equal(matchRegex);
     });
 
-    mocha.it("should export the match-regex rule", function () {
+    it("should export the match-regex rule", function () {
         expect(index.rules['match-exported']).to.equal(matchExported);
     });
 
-    mocha.it("should export the match-regex rule", function () {
+    it("should export the match-regex rule", function () {
         expect(index.rules['no-index']).to.equal(noIndex);
     });
 });
